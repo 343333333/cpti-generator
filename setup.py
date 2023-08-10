8afd9731cb0dac29c06ed06b8d81ea49a46ec70b
Benjamin Wake <38664390+343333333@users.noreply.github.com>
2023-08-10 19:46:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 #readme_file = path.join(path.dirname(path.abspath(__file__)), 'README.md')
 
-install_requires=['numpy>=1.14.3','ase']
+install_requires=['numpy>=1.14.3']
 
 setuptools.setup(
 	name=NAME,
@@ -18,16 +18,16 @@ setuptools.setup(
 	description="Inspired by DPGEN, a workflow to calculate the number\
     of electrons in a constant-potential model is established",
 	python_requires="~=3.6",
-	packages=['qgen',
-			  'qgen/generator',
-			  'qgen/dispatcher',
-			  'qgen/collect',
+	packages=['cpti',
+			  'cpti/generator',
+			  'cpti/dispatcher',
+			  'cpti/collect',
 	],
 	keywords='workflow',
 	install_requires=install_requires,    
 		entry_points={
 		'console_scripts': [
-			SHORT_CMD+'= qgen.main:main']
+			SHORT_CMD+'= cpti.main:main']
    }
 )
 
